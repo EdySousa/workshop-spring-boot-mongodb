ed25a56bbbb8ab3d04e006a28a0d225fc68fdbd6
Edvaldo Segundo <edvaldo.segundo@hotmail.com>
2018-11-21 18:04:24 +0000

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/edysousa/workshopmongo/services/UserService.java
@@ -0,0 +1,22 @@
+package com.edysousa.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.edysousa.workshopmongo.domain.User;
+import com.edysousa.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+		
+	}
+
+}
