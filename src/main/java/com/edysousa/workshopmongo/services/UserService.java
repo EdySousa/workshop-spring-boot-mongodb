4e46b4447bfd50ade05765c5ad98a69914da68c1
Edvaldo Segundo <edvaldo.segundo@hotmail.com>
2018-11-23 10:35:47 +0000

    Inserção de usuário com POST

--- src/main/java/com/edysousa/workshopmongo/services/UserService.java
+++ src/main/java/com/edysousa/workshopmongo/services/UserService.java
@@ -7,23 +7,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.edysousa.workshopmongo.domain.User;
+import com.edysousa.workshopmongo.dto.UserDTO;
 import com.edysousa.workshopmongo.repository.UserRepository;
 import com.edysousa.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
-		
+
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 
 }
